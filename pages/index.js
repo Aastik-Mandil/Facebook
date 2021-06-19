52736df3fc8f25840624699f99465aafa9b37868
Aastik Mandil <astik24mandilkota@gmail.com>
2021-06-19 13:01:04 +0530

    update facebook

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ import Widgets from '../components/Widgets';
 import { db } from "../firebase";
 
 export default function Home({ session, posts }) {
-  // if(!session){
-  //   return <Login />
-  // }
+  if(!session){
+    return <Login />
+  }
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
